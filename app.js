4e94993b21933263d0d7eedb78b645f7309e1348
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 10:30:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Exercise 10
+// My name is Adeel
+// Dated 03/03/2024
+// This program will run simple code just like hello world
+console.log("Hello world simple program");
+// Exercise 10
+// My name is Adeel
+// Dated 03/03/2024
+console.log(5 * 3);
+export {};
---
+++ app.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+
+// Exercise 10
+// My name is Adeel
+// Dated 03/03/2024
+// This program will run simple code just like hello world
+console.log("Hello world simple program");
+
+// Exercise 10
+// My name is Adeel
+// Dated 03/03/2024
+console.log(5*3);
+
